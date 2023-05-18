3296c318cbca4650f9d902f1535e2d812be233f1
Quilvix <sharklydog@mail.ru>
2023-05-19 02:08:50 +0300

    я абсолютно ебал это. тут созданы кнопки для добавления и удаления однако они не работают и я так не понял почему, кнопка для того чтобы дублировать в принципе не хочет никак работать потому как он ругается на то что библиотека которая нужна для этой кнопки не может использоваться в этом проекте

---
+++ Лаб29/ApplicationViewModel.cs
@@ -0,0 +1,104 @@
+﻿using SolrNet.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Лаб29;
+
+namespace Лаб29
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Bus selectedBus;
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Bus bus = new Bus();
+                      Buses.Insert(0, bus);
+                      SelectedBus = bus;
+                  }));
+            }
+        }
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      Bus bus = obj as Bus;
+                      if (bus != null)
+                      {
+                          Buses.Remove(bus);
+                      }
+                  },
+                 (obj) => Buses.Count > 0));
+            }
+        }
+        private RelayCommand doubleCommand;
+        public RelayCommand DoubleCommand
+        {
+            get
+            {
+                return doubleCommand ??
+                    (doubleCommand = new RelayCommand(obj =>
+                    {
+                        Bus bus = obj as Bus;
+                        if (bus != null)
+                        {
+                            Bus busCopy = new Bus
+                            {
+                                Vodila = bus.Vodila,
+                                Busnumber = bus.Busnumber,
+                                Seats = bus.Seats
+                            };
+                            Buses.Insert(0, busCopy);
+                        }
+                    }));
+            }
+        }
+
+
+        public ObservableCollection<Bus> Buses { get; set; }
+        public Bus SelectedBus
+        {
+            get { return selectedBus; }
+            set
+            {
+                selectedBus = value;
+                OnPropertyChanged("SelectedBus");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Buses = new ObservableCollection<Bus>
+            {
+                new Bus {Seats=52, Busnumber=11, Vodila="Phil" },
+                new Bus {Seats=52, Busnumber=8, Vodila ="Nikita" },
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
+
+
+
+
---
+++ Лаб29/Bus.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб29
+{
+    public class Bus : INotifyPropertyChanged
+    {
+        private int seats;
+        private int busnumber;
+        private string vodila;
+
+        public int Seats
+        {
+            get { return seats; }
+            set
+            {
+                seats = value;
+                OnPropertyChanged("Seats");
+            }
+        }
+        public int Busnumber
+        {
+            get { return busnumber; }
+            set
+            {
+                busnumber = value;
+                OnPropertyChanged("Bus number");
+            }
+        }
+        public string Vodila
+        {
+            get { return vodila; }
+            set
+            {
+                vodila = value;
+                OnPropertyChanged("Vodila");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
